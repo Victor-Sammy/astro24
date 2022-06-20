bf315050ad2efe5f4df8f82c82f09411407d7192
mjaumi <mjaumi2864@gmail.com>
2022-06-20 23:01:17 +0600

    description fixed

--- frontend/src/pages/Description/Description/Description.js
+++ frontend/src/pages/Description/Description/Description.js
@@ -22,7 +22,7 @@ const Description = () => {
         <section className='min-h-screen pt-24'>
             <PageTitle title={'Description'} />
             <div className='w-[95%] 2xl:w-4/5 mx-auto my-10 bg-accent shadow-2xl p-3 md:p-5 2xl:p-8 rounded-3xl grid grid-cols-1 2xl:grid-cols-2 gap-10'>
-                <ProductGallery images={gem.images} />
+                <ProductGallery images={gem?.images} />
                 <div className='2xl:text-left mt-3'>
                     <h3 className='text-4xl font-bold text-secondary'>{gem.name}</h3>
                     <div className='mt-3 flex items-center justify-between'>
@@ -52,17 +52,17 @@ const Description = () => {
                             <p className='mb-5 2xl:mb-0 2xl:mr-5'>Quantity:</p>
                             <QuantityChooser />
                         </div>
-                        <div className='block 2xl:flex items-center mt-12'>
-                            <button className='btn btn-primary text-accent capitalize font-normal rounded-xl w-full 2xl:w-48'>
-                                <BiPurchaseTagAlt className='mr-2 text-xl' />
-                                Buy Now
-                            </button>
-                            <button className='btn btn-outline btn-secondary text-accent capitalize font-normal rounded-xl w-full 2xl:w-48 mt-5 2xl:mt-0 2xl:ml-5'>
-                                <BsCartPlus className='mr-2 text-xl' />
-                                Add To Cart
-                            </button>
-                        </div>
                     </div>
+                </div>
+                <div className='block 2xl:grid grid-cols-2 gap-6 w-full'>
+                    <button className='btn btn-primary text-accent capitalize font-normal rounded-xl w-full'>
+                        <BiPurchaseTagAlt className='mr-2 text-xl' />
+                        Buy Now
+                    </button>
+                    <button className='btn btn-outline btn-secondary text-accent capitalize font-normal rounded-xl mt-5 2xl:mt-0 w-full'>
+                        <BsCartPlus className='mr-2 text-xl' />
+                        Add To Cart
+                    </button>
                 </div>
             </div>
         </section>
--- frontend/src/pages/Description/ProductGallery/ProductGallery.js
+++ frontend/src/pages/Description/ProductGallery/ProductGallery.js
@@ -1,29 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const ProductGallery = ({ images }) => {
-    // integration of react hooks here
-    const [index, setIndex] = useState(0);
 
     // rendering product gallery component here
     return (
         <div>
-            <div className='flex flex-col-reverse md:flex-row'>
-                <div className='grid grid-cols-5 gap-2 md:block mt-8 md:mt-0'>
-                    {
-                        images?.map((img, i) => <img
-                            key={i}
-                            src={img}
-                            alt={i}
-                            onClick={() => setIndex(i)}
-                            className={`md:h-24 md:w-24 ${i !== 0 && 'md:mt-5'} rounded-2xl cursor-pointer hover:scale-110 duration-300 ${i === index && 'border-4 border-secondary'}`}
-                        />)
-                    }
-                </div>
-                <div className='md:ml-10 flex-1'>
-                    <img className='w-full rounded-2xl' src={images && images[index]} alt={index} />
-                </div>
-            </div>
+            {
+                images &&
+                <ImageGallery
+                    thumbnailPosition='left'
+                    infinite={false}
+                    showFullscreenButton={false}
+                    showPlayButton={false}
+                    items={images}
+                />
+            }
         </div>
     );
 };
