c95cdca1a9152e6802e5af2bcf968243de721f1d
unknown <imran nazir>
2022-06-27 18:41:50 +0600

    offers img load from backend

--- frontend/src/pages/offer/Offers.js
+++ frontend/src/pages/offer/Offers.js
@@ -1,12 +1,19 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const Offers = () => {
-    const images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const [offers, setOffers] = useState([])
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get("http://localhost:5000/offers")
+            console.log(data);
+            setOffers(data)
+        })()
+    }, [])
     return (
         <div className='pt-28 pb-6 w-[90%] lg:w-[80%] mx-auto flex flex-col gap-6'>
             <p className='text-left text-3xl text-primary'>TODAY'S OFFERS</p>
             {
-                images.map(Image => <img src="https://kinclimg3.bluestone.com/f_webp,c_scale,w_1190,b_rgb:ffffff/product/dynamic_banner/desktop/home/1655146640743-Home_Page_banner_1190_386pxl_14th_June_final.jpg" alt="" />)
+                offers.map(offer => <img src={offer?.img} alt="" />)
             }
         </div>
     );
